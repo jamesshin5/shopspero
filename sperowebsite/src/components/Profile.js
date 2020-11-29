df16e4371504e61fe56e00574bbfabb80c8b2231
James Shin <james.shin@berkeley.edu>
2020-11-28 16:55:40 -0800

    finished staff page layout
    need to add pictures
    
    can change text settings easily in future

--- sperowebsite/src/components/Profile.js
+++ sperowebsite/src/components/Profile.js
@@ -8,6 +8,7 @@ const Profile = (props) => {
         <div>
             <div className='profile-container'>
                 <img src={props.imageURL} alt='Logo' className="profile-img"/>
+                <div class="top-dark">{props.description}</div>
                 <div class="centered-dark">{props.name}</div>
                 <div class="below-dark">{props.title}</div>
             </div>
--- sperowebsite/src/pages/desktop/TeamDesktop.js
+++ sperowebsite/src/pages/desktop/TeamDesktop.js
@@ -8,19 +8,25 @@ const TeamPage = props => {
   return (
     <div className="staff-main-container">
         <div className="staff-card-container">
+              <div className="staff-title">
+                TEAM SPERO
+              </div>
+              <div className="staff-description">
+                Shop Spero is comprised of a diverse group of Christians who all choose to use their talents and skills to glorify God through their work on this team. This staff strives to sharpen one another and inspire others to carry out God’s mission. Meet the staff that makes Shop Spero a reality.  
+              </div>
             <div className="cards-table">
-                <Profile  name="REBECCA SOO" title="PRESIDENT" imageURL={require("../../images/staff/jamesshin.png")} />
-                <Profile name="DORCAS CHEUNG" title="MARKETING LEAD" imageURL={require("../../images/staff/dorcascheung.png")} />
-                <Profile  name="ANDREW CHENG" title="OPERATIONS LEAD" imageURL={require("../../images/staff/jamesshin.png")} />
-                <Profile name="MATTHEW FLORES" title="TECHNICAL LEAD" imageURL={require("../../images/staff/dorcascheung.png")} />
-                <Profile name="JASMINE CHEN" title="DESIGN LEAD" imageURL={require("../../images/staff/dorcascheung.png")} />
+                <Profile  name="REBECCA SOO" title="PRESIDENT" imageURL={require("../../images/staff/jamesshin.png")} description="Hi! I’m Rebecca! I am majoring in Sociology and Media Studies with a minor in Public Policy. In my free time I like finding new snacks at Trader Joe’s and taking pictures of sunsets. I love Spero because it gives us the opportunity to be a light on campus and meet new people :’)"/>
+                <Profile name="DORCAS CHEUNG" title="MARKETING LEAD" imageURL={require("../../images/staff/dorcascheung.png")} description="hello!! I am Dorcas, a Business and Public Policy student at Berkeley. I am passionate about marketing strategies, storytelling, and social impact. I often travel to and from my current home in the Bay Area to my east coast and HongKong roots. You will find me going on a run, planning the next picnic, and listening to podcasts in my free time. I hope that through Spero, we will bring attention to His glory."/>
+                <Profile  name="ANDREW CHENG" title="OPERATIONS LEAD" imageURL={require("../../images/staff/jamesshin.png")} description="What is up!! I'm Andrew, a third year at UC Berkeley studying Data Science and music. I love basketball, running, acappella, magic, camping, smash ultimate, and many other small things in life! I'm serving in Spero because I hope to use fashion as a platform to spread the gospel!"/>
+                <Profile name="MATTHEW FLORES" title="TECHNICAL LEAD" imageURL={require("../../images/staff/dorcascheung.png")} description="Hi! I'm Matt, a third year at UC Berkeley studying Cognitive Science, and I'm the head of Web Design and tech! I love to dance, shop, binge new mock-umentaries/comedies, studying in cafes, and trying all different types of boba and latte drinks! I joined Spero because I could combine my passion for design with something so important to me like my faith!"/>
+                <Profile name="JASMINE CHEN" title="DESIGN LEAD" imageURL={require("../../images/staff/dorcascheung.png")} description=" hi friends (о´∀`о) I'm Jasmine, a second year at Cal studying Architecture! In my free time, I like to go thrifting, play with my dogs, watch korean dramas, and make stickers!! I joined Spero because I wanted to design for a higher purpose, and hopefully learn new ways to think about my faith!" />
             </div>
             <div className="cards-table">
-                <Profile  name="JUSTIN PARK" title="OPERATIONS" imageURL={require("../../images/staff/jamesshin.png")} />
-                <Profile name="JAMES SHIN" title="WEB DEVELOPER" imageURL={require("../../images/staff/dorcascheung.png")} />
-                <Profile  name="JENNY WOO" title="MARKETING" imageURL={require("../../images/staff/jamesshin.png")} />
-                <Profile name="RACHEL SOO" title="DESIGN" imageURL={require("../../images/staff/dorcascheung.png")} />
-                <Profile name="ESTHER YOO" title="MARKETING" imageURL={require("../../images/staff/dorcascheung.png")} />
+                <Profile  name="JUSTIN PARK" title="OPERATIONS" imageURL={require("../../images/staff/jamesshin.png")} description="Hey!  My name is Justin Park, and I am a third year @ UC Berkeley studying cognitive science and data science.  I love singing, going on adventures, driving, and hanging out for hours at the park!  I joined Spero because it is filled with people passionate about God and love to serve God with all of their lives."/>
+                <Profile name="JAMES SHIN" title="WEB DEVELOPER" imageURL={require("../../images/staff/dorcascheung.png")} description="Hello! I'm James Shin, a junior at UC Berkeley studying computer science and data science. I love playing the guitar, wearing big hoodies, binging TV shows like Suits or Mad Men, playing basketball and Fortnite, and working with all the talented and creative people at Spero!"/>
+                <Profile  name="JENNY WOO" title="MARKETING" imageURL={require("../../images/staff/jamesshin.png")} description="Heyhey!! I'm Jenny, a second year at UC Berkeley hoping to major in Public Health! I love photography (like photoshoots with friendzz), coffee, baking, calligraphy, and theology books (heh)! I joined Spero because I wanted to share the gospel through an art form 🙂"/>
+                <Profile name="RACHEL SOO" title="DESIGN" imageURL={require("../../images/staff/dorcascheung.png")} description="Hello y'all! I'm Rachel, a senior at Mission Viejo High School. In my free time, you can find me browsing anything Disney, singing along to my favorite musicals, or drawing funny pictures of my friends! I joined Spero because I want to use my talents to spread the good news and help develop my faith :)"/>
+                <Profile name="ESTHER YOO" title="MARKETING" imageURL={require("../../images/staff/dorcascheung.png")} description=" Hi! I'm Esther, a second year at UC Berkeley studying Econ and Public Policy. I love exploring the Bay Area, drinking coffee, making TikToks, and taking pics. I joined Spero because I really believe in its mission and love giving back to the community (also love the  aesthetic hehe)."/>
             </div>
         </div>
         <Footer color="#fff2e6"/>
