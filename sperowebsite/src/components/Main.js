7a24a0910de1b3f2d158dbe6885af5b7932e212e
James Shin <james.shin@berkeley.edu>
2021-01-23 01:04:53 -0800

    took out scrolltotop and added space on bottom for about page

--- sperowebsite/src/components/Main.js
+++ sperowebsite/src/components/Main.js
@@ -50,62 +50,59 @@ const Main = ({ location }) => {
                     classNames={'fade'}
                 >
                     <section className="route-section">
-                        <ScrollToTop>
-                            <Switch location={location}>
-                                <Route
-                                    exact
-                                    path="/"
-                                    component={withRouter(HomePage)}
-                                />
-                                <Route path="/about" component={AboutPage} />
-                                <Route path="/team" component={TeamPage} />
-                                <Route
-                                    path="/designs"
-                                    component={DesignsPage}
-                                />
-                                <Route
-                                    path="/design/mountainmover"
-                                    component={MountainMoverPage}
-                                />
-                                <Route
-                                    path="/design/walkonwater"
-                                    component={WalkOnWaterPage}
-                                />
-                                <Route
-                                    path="/design/sperobear"
-                                    component={SperoBearPage}
-                                />
-                                <Route
-                                    path="/design/abide"
-                                    component={AbideHoodiePage}
-                                />
-                                <Route
-                                    path="/design/iamthatgirl"
-                                    component={IAmThatGirlHatPage}
-                                />
-                                <Route
-                                    path="/design/hopelongsleeve"
-                                    component={HopePage}
-                                />
-                                <Route
-                                    path="/design/macronhoodie"
-                                    component={MacronHoodiePage}
-                                />
-                                <Route
-                                    path="/design/blessedtobless"
-                                    component={BlessedToBlessPage}
-                                />
-                                <Route
-                                    path="/design/bayarea"
-                                    component={BayAreaPage}
-                                />
-                                <Route
-                                    path="/design/berkeleyhoodie"
-                                    component={BerkeleyHoodiePage}
-                                />
-                                {/* <Route path='/contact' component={ContactPage} /> */}
-                            </Switch>
-                        </ScrollToTop>
+                        {/* <ScrollToTop> */}
+                        <Switch location={location}>
+                            <Route
+                                exact
+                                path="/"
+                                component={withRouter(HomePage)}
+                            />
+                            <Route path="/about" component={AboutPage} />
+                            <Route path="/team" component={TeamPage} />
+                            <Route path="/designs" component={DesignsPage} />
+                            <Route
+                                path="/design/mountainmover"
+                                component={MountainMoverPage}
+                            />
+                            <Route
+                                path="/design/walkonwater"
+                                component={WalkOnWaterPage}
+                            />
+                            <Route
+                                path="/design/sperobear"
+                                component={SperoBearPage}
+                            />
+                            <Route
+                                path="/design/abide"
+                                component={AbideHoodiePage}
+                            />
+                            <Route
+                                path="/design/iamthatgirl"
+                                component={IAmThatGirlHatPage}
+                            />
+                            <Route
+                                path="/design/hopelongsleeve"
+                                component={HopePage}
+                            />
+                            <Route
+                                path="/design/macronhoodie"
+                                component={MacronHoodiePage}
+                            />
+                            <Route
+                                path="/design/blessedtobless"
+                                component={BlessedToBlessPage}
+                            />
+                            <Route
+                                path="/design/bayarea"
+                                component={BayAreaPage}
+                            />
+                            <Route
+                                path="/design/berkeleyhoodie"
+                                component={BerkeleyHoodiePage}
+                            />
+                            {/* <Route path='/contact' component={ContactPage} /> */}
+                        </Switch>
+                        {/* </ScrollToTop> */}
                     </section>
                 </CSSTransition>
             </TransitionGroup>
