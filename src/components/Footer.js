2f301ceb58ee4f71d45e1552465659951f59924a
Christie Lum <christielum@berkeley.edu>
2021-03-14 17:10:50 -0700

    finished footer and fixed bugs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,21 +6,19 @@ import { HStack, Image, Box, Text } from '@chakra-ui/react'
 const Footer = (props) => {
     return (
         <React.Fragment>
-            <div className="footer-container">
+            {/* <div className="footer-container"> */}
             <Image
                     src={require('../images/paintFooter.png')}
                     w="100%"
                     h="200px"
-                    pos="absolute"
-                    bottom="5px"
-                    // paddingTop="50px"
+                    pos="relative"
                         />
-            <Fade bottom>
+            {/* <Fade bottom="30"> */}
                 {/* <div className="footer-blurb">“Are not two sparrows sold for a penny? Yet not one of them 
                     will fall to the ground outside your Father’s care. And even the 
                     very hairs of your head are all numbered.. So don’t be afraid; 
                     you are worth more than many sparrows.” - Matthew 10:29-31</div> */}
-                <Box w="300px" pos="absolute" bottom = "30px" left="50px" transition="width 0.25s">
+                <Box w={{base: "75%", sm: "50%", md: "35%", lg: "25%"}} pos="absolute" bottom = "8" left="10" transition="width 0.25s">
                     <Text
                         fontFamily="Tenor Sans"
                         color="white"
@@ -34,7 +32,7 @@ const Footer = (props) => {
                         you are worth more than many sparrows.” - Matthew 10:29-31
                         </Text>
                 </Box>
-            </Fade>        
+            {/* </Fade>         */}
             <HStack
                 display="flex"
                 position="absolute" /*fixed*/
@@ -46,17 +44,17 @@ const Footer = (props) => {
                 // bgColor="gray.500"
             >
                 <Fade bottom> 
-                    <Box w="30px" _hover={{ w: '27px' }} transition="width 0.25s">
+                    <Box w="30px" _hover={{ w: '34px' }} transition="width 0.25s">
                         <a href="https://www.facebook.com/shopspero/">
                             <Image
                                 src={require('../images/fb_white_icon.png')}
-                                alt="facebook-icon"  //BOX PREVIOUSLY 23px
+                                alt="facebook-icon"  //BOX PREVIOUSLY 23px and 27px for hover
                             />
                         </a>
                     </Box>
                 </Fade>
                 <Fade bottom>
-                    <Box w="30px" _hover={{ w: '27px' }} transition="width 0.25s">
+                    <Box w="30px" _hover={{ w: '34px' }} transition="width 0.25s">
                         <a href="https://www.instagram.com/shopspero/">
                             <Image
                                 src={require('../images/instagram_white_icon.png')}
@@ -66,7 +64,7 @@ const Footer = (props) => {
                     </Box>
                 </Fade>
                 <Fade bottom>
-                    <Box w="30px" _hover={{ w: '27px' }} transition="width 0.25s">
+                    <Box w="30px" _hover={{ w: '34px' }} transition="width 0.25s">
                         <a href="https://shopspero.medium.com/">
                             <Image
                                 src={require('../images/medium_white_icon.png')}
@@ -76,7 +74,7 @@ const Footer = (props) => {
                     </Box>
                 </Fade>
             </HStack>
-            </div>
+            {/* </div> */}
         </React.Fragment>
 
         // <div className="footer-container">
