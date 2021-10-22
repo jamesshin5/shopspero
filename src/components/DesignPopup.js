430de5a8a4ccd8b68aed754e04823299bfa1de29
bluepegasus123 <bluepegasus123@gmail.com>
2021-10-22 16:53:01 -0700

    Added a feature for just outside modal click == modal close

--- src/components/DesignPopup.js
+++ src/components/DesignPopup.js
@@ -3,7 +3,7 @@ import '../styles/DesignPopup.css'
  
 const DesignPopup = props => {
   return (
-    <div className="popup-box">
+    <div ref = {props.reference} className="popup-box">
       <div className="box">
         <span className="close-icon" onClick={props.handleClose}>x</span>
         {props.content}
