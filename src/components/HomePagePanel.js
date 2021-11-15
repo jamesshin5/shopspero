7765268caae586b6681ea09b29cf74c44e51e81f
mk1m <61814632+mk1m@users.noreply.github.com>
2021-11-14 16:09:26 -0800

    Carousel

--- src/components/HomePagePanel.js
+++ src/components/HomePagePanel.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from 'react-router-dom'
-import { Image, Box } from '@chakra-ui/react'
+import { Image, Box, VStack, Text, Link } from '@chakra-ui/react'
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import '../styles/HomePagePanel.css'
@@ -10,6 +10,7 @@ import image1 from "../images/sliderimage2.jpg"
 import image2 from "../images/sliderimage3.jpg"
 import image3 from "../images/sliderimage4.jpg"
 import image4 from "../images/sliderimage5.jpg"
+import { Nav } from "react-bootstrap";
 
 const images = [
     image0, image1, image2, image3, image4
@@ -36,30 +37,47 @@ const HomePagePanel = ({ deviceType }) => {
         }
     };
     return (
-        <Carousel
-            ssr
-            deviceType={deviceType}
-            itemClass="image-item"
-            responsive={responsive}
-            infinite={true}
-            autoPlay={false}
-            centerMode
-        >
-            {images.slice(0, 5).map(image => {
-                return (
-                    <Box w="350px" >
-                        <Image
-                            draggable={false}
-                            style={{ width: "90%", height: "100%" }}
-                            src={image}
-                            alt="home-images"
-                            mt="10%"
-                            objectFit="cover"
-                        />
-                    </Box>
-                );
-            })}
-        </Carousel>
+        <VStack
+            spacing={200}
+            align="stretch">
+            
+
+            <Carousel
+                ssr
+                deviceType={deviceType}
+                itemClass="image-item"
+                responsive={responsive}
+                infinite={true}
+                autoPlay={false}
+                centerMode
+            >
+                {images.slice(0, 5).map(image => {
+                    return (
+                        <Box w="350px">
+                            <Image
+                                draggable={false}
+                                style={{ width: "90%", height: "100%" }}
+                                src={image}
+                                alt="home-images"
+                                objectFit="cover"
+                            />
+                            <NavLink exact to="/designs">
+                                <Box
+                                    style={{ width: "96%", height: "100%" }}
+                                    position="absolute"
+                                    top={0}
+                                    bottom={0}
+                                    bg="black"
+                                    opacity='0.5'
+                                    _hover={{ opacity: '0' }}
+                                >
+                                </Box>
+                            </NavLink>
+                        </Box>
+                    );
+                })}
+            </Carousel>
+        </VStack >
     );
 }
 
