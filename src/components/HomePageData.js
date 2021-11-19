0a36977dbb935e8f766dcb1ea05a10df2ea588b1
mk1m <61814632+mk1m@users.noreply.github.com>
2021-11-19 02:17:23 -0800

    Create HomePageData.js

---
+++ src/components/HomePageData.js
@@ -0,0 +1,22 @@
+export const SliderData = [
+    {
+        image: require("../images/sliderimage1.jpg"),
+        description: "new arrivals."
+    },
+    {
+        image: require("../images/sliderimage2.jpg"),
+        description: "our mission."
+    },
+    {
+        image: require("../images/sliderimage3.jpg"),
+        description: "shop now."
+    },
+    {
+        image: require("../images/sliderimage4.jpg"),
+        description: "meet the team."
+    },
+    {
+        image: require("../images/sliderimage5.jpg"),
+        description: "contact us."
+    },
+]
