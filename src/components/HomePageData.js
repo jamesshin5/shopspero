47aa86d1042e6bf186e44b4eba14a477c7fecd08
mk1m <61814632+mk1m@users.noreply.github.com>
2021-11-19 16:28:20 -0800

    Home page slider final

--- src/components/HomePageData.js
+++ src/components/HomePageData.js
@@ -1,22 +1,27 @@
 export const SliderData = [
     {
         image: require("../images/sliderimage1.jpg"),
-        description: "new arrivals."
+        description: "new arrivals.",
+        link: "/designs"
     },
     {
         image: require("../images/sliderimage2.jpg"),
-        description: "our mission."
+        description: "our mission.",
+        link: "/about"
     },
     {
         image: require("../images/sliderimage3.jpg"),
-        description: "shop now."
+        description: "shop now.",
+        link: "/shop"
     },
     {
         image: require("../images/sliderimage4.jpg"),
-        description: "meet the team."
+        description: "meet the team.",
+        link: "/team"
     },
     {
         image: require("../images/sliderimage5.jpg"),
-        description: "contact us."
+        description: "contact us.",
+        link: "/team"
     },
 ]
--- src/components/HomePagePanel.js
+++ src/components/HomePagePanel.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { NavLink } from 'react-router-dom'
-import { Image, Box, HStack, Text, VStack, Grid, Link } from '@chakra-ui/react'
+import { Image, Box, HStack, Text, VStack, Link } from '@chakra-ui/react'
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import Fade from 'react-reveal/Fade'
 
 import image0 from "../images/sliderimage1.jpg"
 import image1 from "../images/sliderimage2.jpg"
@@ -13,19 +14,16 @@ import image4 from "../images/sliderimage5.jpg"
 import { SliderData } from './HomePageData'
 import { findByPlaceholderText } from "@testing-library/dom";
 
-const images = [
-    image0, image1, image2, image3, image4
-]
 
 const HomePagePanel = ({ deviceType }) => {
     const responsive = {
         largeDesktop: {
-            breakpoint: { max: 4000, min: 1500 },
+            breakpoint: { max: 4000, min: 1600 },
             items: 5,
             partialVisibilityGutter: 40
         },
         desktop: {
-            breakpoint: { max: 1500, min: 1300 },
+            breakpoint: { max: 1600, min: 1300 },
             items: 4,
             partialVisibilityGutter: 40
         },
@@ -34,9 +32,14 @@ const HomePagePanel = ({ deviceType }) => {
             items: 3,
             partialVisibilityGutter: 40
         },
-        tablet: {
-            breakpoint: { max: 1024, min: 500 },
+        largeTablet: {
+            breakpoint: { max: 1024, min: 700 },
             items: 2,
+            partialVisibilityGutter: 10
+        },
+        tablet: {
+            breakpoint: { max: 700, min: 500 },
+            items: 1.5,
             partialVisibilityGutter: 10
         },
         mobile: {
@@ -48,50 +51,55 @@ const HomePagePanel = ({ deviceType }) => {
 
     const slides = SliderData.map(slide => {
         return (
-            <Box >
-            <Image
-                draggable={false}
-                style={{ width: "95%", height: "100%" }}
-                src={slide.image}
-                marginLeft="2.5%"
-                alt="home-images"
-                objectFit="cover"
-            />
-            <Box
-                style={{ width: "95%", height: "100%" }}
-                position="absolute"
-                marginLeft="2.5%"
-                top={0}
-                bottom={0}
-                bg="black"
-                opacity='0.5'
-                _hover={{ opacity: '0' }}
-            >
-                <Text
-                    marginTop="20%"
-                    fontFamily="Lexend Deca"
-                    fontWeight="500"
-                    fontSize="6xl"
-                    color="#ffffff"
-                    transition="font-size 0.5s"
-                    align="center"
-                >
-                    {slide.description}
-                </Text>
+            <Box marginTop="5%">
+                <Image
+                    draggable={false}
+                    style={{ width: "95%", height: "100%" }}
+                    src={slide.image}
+                    marginLeft="3%"
+                    alt="home-images"
+                    objectFit="cover"
+                />
+                <NavLink to={slide.link}>
+                    <Box
+                        style={{ width: "95%", height: "100%" }}
+                        position="absolute"
+                        marginLeft="3%"
+                        marginTop="5%"
+                        top={0}
+                        bottom={0}
+                        bg="black"
+                        opacity='0.5'
+                        _hover={{ opacity: '0' }}
+                    >
+                        <Fade bottom>
+                            <Text
+                                marginTop="40%"
+                                fontFamily="Josefin Sans"
+                                fontWeight="500"
+                                fontSize="6xl"
+                                color="#ffffff"
+                                transition="font-size 0.5s"
+                                align="center"
+                            >
+                                {slide.description}
+                            </Text>
+                        </Fade>
+                    </Box>
+                </NavLink>
             </Box>
-        </Box>
         )
     })
     return (
         <Carousel
-                ssr
-                deviceType={deviceType}
-                removeArrowOnDeviceType={["largeDesktop", "desktop", "smallDesktop", "tablet", "mobile"]}
-                itemClass="carousel-item-padding-0-px"
-                responsive={responsive}
-                draggable={false}
-            >
-                {slides}
+            ssr
+            deviceType={deviceType}
+            itemClass="carousel-item-padding-0-px"
+            responsive={responsive}
+            draggable={false}
+            infinite={true}
+        >
+            {slides}
         </Carousel>
     );
 }
